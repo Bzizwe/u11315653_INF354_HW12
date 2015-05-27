8d799beeffc663b3098754f42fdb15bfe3254a67
Bzizwe <bzizwe_hlophe@yahoo.com>
2015-05-27 13:26:30 +0200

    Create Prac.php

---
+++ Prac.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+<div style = “color:red”>
+define("ZBL MABUNDA u11123738");
+echo constant("ZBL MABUNDA u11123738");
+<div>
+<div style = “color:blue”>
+define("MMC Moraba u29635030");
+echo constant("MMC Moraba u29635030");
+<div>
+
+<div style = “color:brown”>
+define("R Singo u11059584");
+echo constant("R Singo u11059584");
+<div>
+<div style = “color:green”>
+define("B Hlophe u11315653");
+echo constant("B Hlophe u11315653");
+<div>
+?>
+
+</body>
+</html>
